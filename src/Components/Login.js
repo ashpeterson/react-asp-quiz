0de00f4f901dd7460db4d3cb1e39b7be3e27c8d3
ashpeterson <ashpeterson177@gmail,.com>
2024-01-06 00:52:37 +0000

    Added login js.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,43 @@
-import { TextField } from '@mui/material'
+import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import React from 'react'
+import Center from './Center'
 
 export default function Login() {
   return (
-    <form noValidate>
-        <TextField
-        label="Email"
-        name='Email'
-        variant='outlined'
-        ></TextField>
-    </form>
+    <Center>
+        <Card sx= {{ width: 400 }}>
+            <CardContent sx={{textAlign:"Center"}}>
+                <Typography variant="h3" sx={{my:4}}>
+                    Quiz App
+                </Typography>
+            <Box sx= {{
+                '& .MuiTextField-root': {
+                margin: 1,
+                width:'90%'
+            }
+                }} > 
+            <form noValidate>
+                <TextField
+                    label="Email"
+                    name='email'
+                    variant='outlined'/>
+
+                <TextField
+                    label="Name"
+                    name='name'
+                    variant='outlined'/>
+                
+                <Button     
+                    type="submit"
+                    variant='contained'
+                    size='large'
+                    sx={{width:'90%'}}>Start</Button>
+            </form>
+        </Box>
+            </CardContent>
+        </Card>
+    </Center>
+
+
   )
 }
