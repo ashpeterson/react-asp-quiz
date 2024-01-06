d206b4f7c7694d1863b2aff080b40d6f2df4cdbb
ashpeterson <ashpeterson177@gmail,.com>
2024-01-06 13:30:53 +0000

    Added validation and api support.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,31 +1,73 @@
 import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import React from 'react'
 import Center from './Center'
+import useForm from '../Hooks/useForms'
+import { ENDPOINTS, createAPIEndpoint } from '../api'
+
+const getFreshModelObject = ()=> ({
+    name: '',
+    email:''
+})
 
 export default function Login() {
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange
+    } = useForm(getFreshModelObject);
+
+const login = e =>{
+    e.preventDefault();
+    if(validate())
+        createAPIEndpoint(ENDPOINTS.participant)
+        .post(values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+} 
+
+const validate = ()=>{
+    let temp = {}
+    temp.email = (/\S+@\S+\.\S+/).test(values.email)?"":"Email is not valid."
+    temp.name = values.name!=""?"":"This field is required."
+    setErrors(temp)
+    return Object.values(temp).every(x=> x=="")
+}
+
   return (
     <Center>
         <Card sx= {{ width: 400 }}>
+
             <CardContent sx={{textAlign:"Center"}}>
+
                 <Typography variant="h3" sx={{my:4}}>
                     Quiz App
                 </Typography>
+
             <Box sx= {{
                 '& .MuiTextField-root': {
                 margin: 1,
                 width:'90%'
             }
                 }} > 
-            <form noValidate>
+            <form noValidate onSubmit={login}>
                 <TextField
                     label="Email"
                     name='email'
-                    variant='outlined'/>
+                    value={values.email}
+                    onChange={handleInputChange}
+                    variant='outlined'
+                    {...(errors.email && {error:true, helperText:errors.email})}/>
 
                 <TextField
                     label="Name"
                     name='name'
-                    variant='outlined'/>
+                    value={values.name}
+                    onChange={handleInputChange}
+                    variant='outlined'
+                    {...(errors.name && {error:true, helperText:errors.name})}/>
                 
                 <Button     
                     type="submit"
---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const BASE_URL = 'http://localhost:5244/';
+
+export const ENDPOINTS = {
+    participant: 'participant'
+}
+
+export const createAPIEndpoint  = endpoint => {
+
+    let url = BASE_URL + 'api/' + endpoint + '/';
+
+    return {
+        fetch: () => axios.get(url),
+        fetchById: id => axios.get(url + id),
+        post: newRecord => axios.post(url, newRecord),
+        put: (id, updatedRecord) => axios.put(url, + id, updatedRecord),
+        delete: id => axios.delete(url + id),
+    }
+}
