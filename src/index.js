f69092e2f813f80a18a99802c2e492a278a53d83
ashpeterson <ashpeterson177@gmail,.com>
2024-01-06 13:30:11 +0000

    Added dark theme.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
+const darkTheme = createTheme({
+  palette:{
+    mode:'dark',
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
