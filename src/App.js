7eb005b1817d566b5be1b8ad1f544f4a98b0b335
ashpeterson <ashpeterson177@gmail,.com>
2024-01-08 19:24:35 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import Login from './Components/Login';
 import Quiz from './Components/Quiz';
 import Result from './Components/Result';
+import Layout from './Components/Layout';
+import Authenticate from './Components/Authenticate';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>} />
-      <Route path="/quiz" element={<Quiz/>} />
-      <Route path="/result" element={<Result/>} />
+      <Route element = {<Authenticate/>}>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/quiz" element={<Quiz/>} />
+          <Route path="/result" element={<Result/>} />
+        </Route>
+      </Route>
     </Routes>
     </BrowserRouter>
   );
