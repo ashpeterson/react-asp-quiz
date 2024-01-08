4df66eb7e076f7bd473f1033d94e1b872069a53f
ashpeterson <ashpeterson177@gmail,.com>
2024-01-08 19:25:14 +0000

    Added font

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const darkTheme = createTheme({
   palette:{
     mode:'dark',
   },
+  typography:{
+    fontFamily:'"IBM Plex Sans"'
+  }
 });
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
