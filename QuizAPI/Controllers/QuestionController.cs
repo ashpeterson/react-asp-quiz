9b2586e862cf5bb57769579f4a2ea0a127b3613f
ashpeterson <ashpeterson177@gmail,.com>
2024-01-08 19:39:10 +0000

    Add project files.

---
+++ QuizAPI/Controllers/QuestionController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using QuizAPI.Models;
+
+namespace QuizAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly QuizDbContext _context;
+
+        public QuestionController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Question
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            var randomFiveQuestions = await (_context.Questions
+                .Select(x => new
+                {
+                    questionID = x.QuestionId,
+                    question = x.QuestionInWords,
+                    imageName = x.ImageName,
+                    Options = new string[] {x.Option1, x.Option2, x.Option3, x.Option4}
+
+                })
+                .OrderBy(y => Guid.NewGuid())
+                .Take(5)
+                ).ToListAsync();
+
+            //return await _context.Questions.ToListAsync();
+            return Ok(randomFiveQuestions);
+        }
+
+        // GET: api/Question/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }
+
+        // PUT: api/Question/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestion(int id, Question question)
+        {
+            if (id != question.QuestionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Question/GetAnswers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("GetAnswers")]
+        public async Task<ActionResult<Question>> RetrieveAnswers(int[] questionIds)
+        {
+            var answers = await (_context.Questions
+                .Where(x => questionIds.Contains(x.QuestionId))
+                .Select(y => new
+                {
+                    questionID = y.QuestionId,
+                    question = y.QuestionInWords,
+                    imageName = y.ImageName,
+                    Options = new string[] { y.Option1, y.Option2, y.Option3, y.Option4 },
+                    Answer = y.Answer
+                })).ToListAsync();
+
+            return Ok(answers);
+        }
+
+        // DELETE: api/Question/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.QuestionId == id);
+        }
+    }
+}
---
+++ QuizAPI/Models/QuizDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizAPI.Models
+{
+    public class QuizDbContext : DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> contextOptions) : base (contextOptions)
+        { }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+    }
+}
